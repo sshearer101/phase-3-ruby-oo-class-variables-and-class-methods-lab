16aef1d2d033af45be37124c1fe8e26ddcc788da
Sam Shearer <sshearer101@gmail.com>
2021-09-01 17:34:20 -0600

    commit tree 5c489605b52d4b3ad611ca625b4a65cee1edbcbb

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artist
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genre.uniq
+    end 
+
+end
